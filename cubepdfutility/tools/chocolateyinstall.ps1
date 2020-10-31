160b72188d9f56a324d0a3403d6242c27e89e8ee
MaySoMusician <maysomusician@gmail.com>
2020-10-31 13:11:40 +0900

    Take over from KageShiron's repository

---
+++ cubepdfutility/tools/chocolateyinstall.ps1
@@ -0,0 +1,47 @@
+﻿# Taken over from https://github.com/KageShiron/chocolatey-packages/blob/9604eafb1b63c34978ac78f1ec17a68c1d4661c2/cubepdfutility/tools/chocolateyinstall.ps1
+# by KageShiron licensed under MIT License
+
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'cubepdfutility'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://dl.cube-soft.jp/archive/cubepdf-utility-0.6.3b.exe'
+$url64      = 'https://dl.cube-soft.jp/archive/cubepdf-utility-0.6.3b-x64.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'cubepdfutility*'
+
+  # Checksums are calculated with the binaries downloaded from the official website, not the GitHub repository
+  checksum      = 'e8be455b1afe3d32d88679fc0c5188047e29189ac37c10c5ebbd1e8419243ddf'
+  checksumType  = 'sha256'
+  checksum64    = '826eabf72b31513197b3d94c16244aa5d04156b025aa3d06a24db87375b68818'
+  checksumType64= 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ cubepdfutility/update.ps1
@@ -0,0 +1,36 @@
+# Taken over from https://github.com/KageShiron/chocolatey-packages/blob/9604eafb1b63c34978ac78f1ec17a68c1d4661c2/cubepdfutility/update.ps1
+# by KageShiron licensed under MIT License
+
+function GetLatestFromReleases($releases) {
+    foreach ($r in $releases[0]) {
+        if($r.name -match "CubePDF Utility \d.\d.\d") {
+            return $r
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $releases = @(Invoke-WebRequest https://api.github.com/repos/cube-soft/Cube.Pdf/releases -UseBasicParsing | ConvertFrom-Json)
+    $version = (GetLatestFromReleases $releases).name.Replace("CubePDF Utility ", "").Replace(" beta", "b")
+
+    return @{ 
+        Version = $version;
+        URL32 = ("https://dl.cube-soft.jp/archive/cubepdf-utility-{0}.exe" -F $version);
+        URL64 = ("https://dl.cube-soft.jp/archive/cubepdf-utility-{0}-x64.exe" -F $version);
+        ChecksumType32 = 'sha256'
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"           #1
+            "(?i)(^\s*checksum\s*=\s*)'.*'"   = "`$1'$($Latest.Checksum32)'"      #2
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"           #1
+            "(?i)(^\s*checksum64\s*=\s*)'.*'"   = "`$1'$($Latest.Checksum64)'"      #2
+        }
+    }
+}
+
+
+Update-Package
